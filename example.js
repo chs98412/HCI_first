a9215bce929fffc5b2d72f8ec13fa1e95e6486b5
최혁순 <hyuk@choehyeogsun-ui-MacBookAir.local>
2021-04-05 23:19:44 +0900

    modi

--- example.js
+++ example.js
@@ -1,73 +1,141 @@
 var canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d');
-    key = {right:false};
-    player={x:50,y:400,width:50,height:80};
-    jump=0;
+
+    function Drawback(color,fill,context){
+        //배경
+        this.color=color;
+        this.fill=fill;
+        this.context=context;
+    }
+    Drawback.prototype={
+
+        rect: function(x,y,w,h){
+            if(this.fill==0){
+                this.context.strokeStyle=this.color;
+                this.context.beginPath();
+                this.context.strokeRect(x,y,w,h);
+
+            }
+            else if(this.fill==1){
+                this.context.fillStyle=this.color;
+                this.context.beginPath();
+                this.context.fillRect(x,y,w,h);
+
+            }
+            else{
+                this.context.fillStyle=this.color;
+                this.context.beginPath();
+                this.context.fillRect(x,y,w,h);
+                this.context.fillStyle='#020202';
+                this.context.strokeRect(x,y,w,h);
 
 
+            }
 
+        },
+        circle: function(x,y,rad){
+            if(this.fill==0){
+                this.context.strokeStyle=this.color;
+                this.context.beginPath();
+                this.context.arc(x,y,rad,Math.PI,Math.PI*3);
+                this.context.stroke();
+            }
+            else{
+                this.context.fillStyle=this.color;
+                this.context.beginPath();
+                this.context.arc(x,y,rad,Math.PI,Math.PI*3);
+                this.context.fill();
+            }
+        },
 
-    setInterval(function(){
-        update();
-        draw();
-      },10)
-      
-      
-      
+        line: function(x1,y1,x2,y2,width){
+            this.context.lineWidth=width;
+            this.context.strokeStlye=this.color;
+            this.context.beginPath();
+            this.context.moveTo(x1,y1);
+            this.context.lineTo(x2, y2);
+            this.context.stroke();
+        },
 
+        text: function(text,x,y){
+            this.context.font ='12px Helvetica';
+            this.context.fillStyle=this.color;
+            this.context.fillText(text,x,y);      
+        }
+    }
 
+    context.strokeStyle='#020202';
+    context.fillStyle='#a03f4e';
+    
 
-
-
-    context.strokeStyle='black';
-
-    context.strokeRect(25, 25, 300, 500);
+    context.fillRect(25, 25, 500, 300);
     context.stroke();
+    context.fill();
+brown = new Drawback('#c3580d',3,context);
+
+brown.rect(100,70,100,30);
+brown.rect(200,80,100,20);
+brown.rect(100,100,200,200);
+
+white = new Drawback('#eeece0',3,context);
+
+white.rect(110,110,85,25);
+white.rect(100,135,200,15);
+for(i=0;i<5;i++)white.rect(100,150+i*30,200,30);
+
+white.rect(350,40,100,15);
+
+gray= new Drawback('#a2a2a2',3,context);
 
 
-    function draw(){
-        context.save();
+for(i=0;i<4;i++)gray.rect(105,190+30*i,10,10);
 
-        context.fillStyle = "white";
-  context.fillRect(26,26,299,499);
+for(i=0;i<5;i++)gray.circle(140,165+i*30,7);
 
 
+bar = new Drawback('#5b5a4c',2,context);
+
+bar.rect(60,40,60,15);
+bar.rect(140,40,80,15);
+bar.rect(230,40,100,15);
+
+cyon = new Drawback('#12afb9',1,context);
+cyon.circle(400,90,10);
+cyon.rect(400,80,70,20);
+cyon.circle(470,90,10);
+cyon.rect(370,240,70,20);
+cyon.circle(370,250,10);
+cyon.circle(440,250,10);
+cyon.circle(500,47,7);
 
 
-        context.strokeStyle='blue';
-        context.strokeRect(player.x, player.y, player.width,player.height);
-        context.stroke();
+green = new Drawback('#09c813',1,context);
+green.circle(220,47,7);
+green.circle(330,47,7);
+green.circle(450,47,7);
 
-        context.restore();
-
-
-      }
-      function update(){
-      if(jump==2)
-        {if(player.y>100)
-            player.y-=20;
-        else if(player.y==100){
-            console.log(jump);
-        jump=3;  }
-    }
-    if(jump==3){
-        if(player.y<400)
-             player.y+=20;
-        else if(player.y==400){
-            jump=0;
-            console.log(jump);}
-    }
+for(i=0;i<4;i++){
+green.circle(270,195+i*30,7);
+green.rect(270,188+i*30,20,14);
+green.circle(290,195+i*30,7);
 }
+green.circle(370,290,10);
+green.rect(370,280,70,20);
+green.circle(440,290,10);
 
+brown.rect(100,300,200,23);
 
+dkbrown = new Drawback('#712e00',1,context);
+dkbrown.rect(210,85,30,10);
+dkbrown.rect(260,85,30,10);
 
-    window.addEventListener("keydown",(e)=>{
-        if(jump==0)
-            jump=1;
-        console.log(e.key)
-       if(e.key=='ArrowRight')
-       key.right=true;
-    });
-    window.addEventListener("keyup",(e)=>{if(jump==1)jump=2;});
-    
-    
+dkbrown.rect(205,110,85,25);
+dkbrown.rect(115,305,35,12);
+dkbrown.rect(160,305,40,8);
+
+ck = new Drawback('#eca009',1,context);
+ck.circle(410,150,16);
+ck.rect(401,160,18,30);
+ck.rect(398,190,7,20);
+ck.rect(398,190,7,20);
+
--- second.js
+++ second.js
@@ -3,6 +3,74 @@ var canvas = document.getElementById('canvas'),
     iconwidth=42;
     distance=5;
 
+    var Point = function (x, y) {
+        this.x = x;
+        this.y = y;
+     };
+     
+     var Polygon = function (centerX, centerY, radius, sides, startAngle, strokeStyle, fillStyle) {
+        this.x = centerX;
+        this.y = centerY;
+        this.radius = radius;
+        this.sides = sides;
+        this.startAngle = startAngle;
+        this.strokeStyle = strokeStyle;
+        this.fillStyle = fillStyle;
+     };
+     
+     Polygon.prototype = {
+        getPoints: function () {
+           var points = [],
+               angle = this.startAngle || 0;
+     
+           for (var i=0; i < this.sides; ++i) {
+              points.push(new Point(this.x + this.radius * Math.sin(angle),
+                                this.y - this.radius * Math.cos(angle)));
+              angle += 2*Math.PI/this.sides;
+           }
+           return points;
+        },
+     
+        createPath: function (context) {
+           var points = this.getPoints();
+     
+           context.beginPath();
+     
+           context.moveTo(points[0].x, points[0].y);
+     
+           for (var i=1; i < this.sides; ++i) {
+              context.lineTo(points[i].x, points[i].y);
+           }
+     
+           context.closePath();
+        },
+     
+        stroke: function (context) {
+           context.save();
+           this.createPath(context);
+           context.strokeStyle = this.strokeStyle;
+           context.stroke();
+           context.restore();
+        },
+     
+        fill: function (context) {
+           context.save();
+           this.createPath(context);
+           context.fillStyle = this.fillStyle;
+           context.fill();
+           context.restore();
+        },
+     
+       
+     };
+ 
+     
+
+
+
+
+
+
 
 function drawback(){
     //배경
@@ -11,11 +79,68 @@ function drawback(){
     
 
     context.fillRect(25, 25, 500, 300);
-  
+
+    context.strokeStyle='#9d9d9d';
+
+    context.strokeRect(80, 35, 50, 20);
+    context.strokeRect(150, 35, 70, 20);
+    context.strokeRect(240, 35, 120, 20);
+
+    context.fillStyle='#f2f2f2';
+    context.fillRect(370, 35, 120, 20);
+    context.fillStyle='#f4e111';
+    context.beginPath();
+    context.arc(78, 44, 8, Math.PI , Math.PI*3 );
+    context.fill();
+
+    context.fillStyle='#a8a8a5';
+    context.beginPath();
+    context.arc(238, 44, 8, Math.PI , Math.PI*3 );
+    context.fill();
+
+    context.fillStyle='#46bd14';
+    context.beginPath();
+    context.arc(220, 44, 8, Math.PI , Math.PI*3 );
+    context.fill();
+    context.beginPath();
+    context.arc(360, 44, 8, Math.PI , Math.PI*3 );
+    context.fill();
+    context.beginPath();
+    context.arc(490, 44, 8, Math.PI , Math.PI*3 );
+    context.fill();
+
+    context.fillStyle='#f83333';
+    context.strokeStyle='#020202';
+    
+    for(i=0;i<=5;i++){
+    context.beginPath();
+    context.arc(420+i*(3+8), 44, 8, Math.PI , Math.PI*3 );
+    context.fill();
+context.stroke();
+    }
+
+context.font ='18px Helvetica';
+context.fillStyle='white';
+
+context.fillText("53%",90,50);  
+context.fillText("923",180,50);  
+context.fillText("15,460",270,50);  
+context.fillStyle='#020202';
+
+context.fillText("13",380,50); 
 }
 drawback();
 
-context.fillStyle='#e3e3e1';
+context.beginPath();
+
+
+context.font ='12px Helvetica';
+context.fillStyle='white';
+
+
+
+
+context.fillStyle='#e3e3e1';//왼쪽
 context.fillRect(80, 70, 170, 200);
 context.fillStyle='#2082e7';
 context.fillRect(95-distance, 100-distance, iconwidth*3+distance*4, iconwidth+distance*2);
@@ -28,9 +153,131 @@ context.fillStyle='#4cdf4f';
 for(i=0;i<3;i++) context.fillRect(95+i*(iconwidth+distance), 100+iconwidth+6*distance, iconwidth, iconwidth);
 for(i=0;i<3;i++) context.fillRect(95+i*(iconwidth+distance), 100+2*iconwidth+7*distance, iconwidth, iconwidth);
 
-context.strokeStyle='#446ff4';
+context.strokeStyle='#4d51fa';//오른쪽
+context.fillStyle='#50a0f2';
 context.strokeRect(80+170+10*distance,70+4*distance,170,150);
+context.fillRect(80+170+13*distance, 70+7*distance, iconwidth, iconwidth);//아이템
+context.fillText("Lv.2",80+170+15*distance, 70+10*distance);   
+
+
+context.fillStyle='#b3b3b7';
+context.arc(80+170+20*distance+iconwidth, 70+12*distance, 5, Math.PI,Math.PI*3);//동전
+context.fill();
+context.font ='18px Helvetica';
+context.fillStyle='white';
+
+context.fillText("체력강화 Lv.2",250+15*distance+iconwidth, 70+9*distance);  
+
+
+context.font ='12px Helvetica';
+
+context.fillText("800",80+170+22*distance+iconwidth, 70+13*distance);   
+context.fillText("Lv.2",80+170+15*distance, 70+14*distance);   
+
+
+
+  
+
+
+
+context.beginPath();//업그레이드버튼
+context.fillStyle='#00dce3';
+context.arc(250+17*distance+iconwidth, 70+18*distance, 10, Math.PI,Math.PI*3);
+context.arc(300+17*distance+iconwidth, 70+18*distance, 10, Math.PI,Math.PI*3);
+context.fill();
+context.fillRect(250+17*distance+iconwidth,70+18*distance-10,50,20);
+context.font ='12px Helvetica';
+context.fillStyle='white';
+context.fillText("업그레이드",250+17*distance+iconwidth,70+18*distance+4);   
+context.fillText("추가체력이 총 10 늘어납니다.",250+5*distance+iconwidth,70+26*distance+4);   
+context.fillText("모든 쿠키들에게 적용됩니다.",250+5*distance+iconwidth,70+29*distance+4);   
 
 
 
 
+
+context.beginPath();//게임시작버튼
+context.fillStyle='#54e110';
+context.arc(330, 265, 16, Math.PI,Math.PI*3);
+context.arc(430,265, 16, Math.PI,Math.PI*3);
+context.fill();
+context.fillRect(330, 265-16,100,32);
+
+
+context.font ='24px Helvetica';
+context.fillStyle='white';
+context.fillText("게임 시작",336, 272);   
+
+
+
+context.fillStyle='#020202';
+context.font ='15px Helvetica';
+context.fillText("능력을 업그레이드하세요",85,90); 
+context.fillText("부스트를 구매하세요",85,163); 
+
+context.fillStyle='#8d8d8d';
+    context.beginPath();
+    context.arc(250, 75, 9, Math.PI , Math.PI*3 );
+    context.fill();
+
+    canvas.onmousedown = function (i) {
+        var loc = windowToCanvas(i.clientX, i.clientY);
+     
+        i.preventDefault(); 
+        context.beginPath();
+        
+        context.arc(330, 265, 16, Math.PI,Math.PI*3);
+        context.arc(430,265, 16, Math.PI,Math.PI*3);
+        context.rect(330, 265-16,100,32);
+        if (context.isPointInPath(loc.x, loc.y)) {
+            console.log("띠용");
+
+            
+         }
+                
+    };
+    
+
+
+        function windowToCanvas(x, y) { //실좌표계산
+            var bbox = canvas.getBoundingClientRect();
+            return { x: x - bbox.left * (canvas.width  / bbox.width),
+                     y: y - bbox.top  * (canvas.height / bbox.height)
+                   };
+         }
+         
+
+
+var a= new Polygon(210,193,15,6,Math.PI*(1/3),'#ddac10','#ddac10');
+
+context.beginPath();
+a.createPath(context);
+a.fill(context);
+
+var b= new Polygon(210,240,15,8,Math.PI*(1/3),'#e40ca2','#e40ca2');
+
+context.beginPath();
+b.createPath(context);
+b.fill(context);
+
+var c= new Polygon(160,193,15,24,Math.PI*(1/3),'#f2f6db','#f2f6db');
+
+context.beginPath();
+c.createPath(context);
+c.fill(context);
+var d= new Polygon(160,240,15,3,Math.PI*(1/3),'#35c5c9','#35c5c9');
+
+context.beginPath();
+d.createPath(context);
+d.fill(context);
+var e= new Polygon(116,193,15,5,Math.PI*(1/3),'#725df2','#725df2');
+
+context.beginPath();
+e.createPath(context);
+e.fill(context);
+
+var f= new Polygon(116,240,15,4,Math.PI*(1/3),'#9cf4fe','#9cf4fe');
+
+context.beginPath();
+f.createPath(context);
+f.fill(context);
