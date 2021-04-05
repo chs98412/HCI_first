a644eff1b1308d09471809e16d4264d42c20d926
ChoiHyukSoon <chs98412@gmail.com>
2021-04-06 00:58:06 +0900

    qwe

--- example.js
+++ example.js
@@ -26,7 +26,7 @@ var canvas = document.getElementById('canvas'),
                 this.context.fillStyle=this.color;
                 this.context.beginPath();
                 this.context.fillRect(x,y,w,h);
-                this.context.fillStyle='#020202';
+                this.context.strokeStyle='#020202';
                 this.context.strokeRect(x,y,w,h);
 
 
@@ -40,11 +40,22 @@ var canvas = document.getElementById('canvas'),
                 this.context.arc(x,y,rad,Math.PI,Math.PI*3);
                 this.context.stroke();
             }
+            else if(this.fill==1){
+                this.context.fillStyle=this.color;
+                this.context.beginPath();
+                this.context.arc(x,y,rad,Math.PI,Math.PI*3);
+                this.context.fill();
+
+            }
             else{
                 this.context.fillStyle=this.color;
                 this.context.beginPath();
                 this.context.arc(x,y,rad,Math.PI,Math.PI*3);
                 this.context.fill();
+                this.context.strokeStyle='#020202';
+                this.context.stroke();
+                
+
             }
         },
 
@@ -83,7 +94,6 @@ white.rect(110,110,85,25);
 white.rect(100,135,200,15);
 for(i=0;i<5;i++)white.rect(100,150+i*30,200,30);
 
-white.rect(350,40,100,15);
 
 gray= new Drawback('#a2a2a2',3,context);
 
@@ -93,11 +103,7 @@ for(i=0;i<4;i++)gray.rect(105,190+30*i,10,10);
 for(i=0;i<5;i++)gray.circle(140,165+i*30,7);
 
 
-bar = new Drawback('#5b5a4c',2,context);
 
-bar.rect(60,40,60,15);
-bar.rect(140,40,80,15);
-bar.rect(230,40,100,15);
 
 cyon = new Drawback('#12afb9',1,context);
 cyon.circle(400,90,10);
@@ -106,18 +112,14 @@ cyon.circle(470,90,10);
 cyon.rect(370,240,70,20);
 cyon.circle(370,250,10);
 cyon.circle(440,250,10);
-cyon.circle(500,47,7);
 
 
 green = new Drawback('#09c813',1,context);
-green.circle(220,47,7);
-green.circle(330,47,7);
-green.circle(450,47,7);
 
-for(i=0;i<4;i++){
-green.circle(270,195+i*30,7);
-green.rect(270,188+i*30,20,14);
-green.circle(290,195+i*30,7);
+for(i=0;i<5;i++){
+green.circle(270,165+i*30,7);
+green.rect(270,158+i*30,20,14);
+green.circle(290,165+i*30,7);
 }
 green.circle(370,290,10);
 green.rect(370,280,70,20);
@@ -126,20 +128,88 @@ green.circle(440,290,10);
 brown.rect(100,300,200,23);
 
 dkbrown = new Drawback('#712e00',1,context);
-dkbrown.rect(210,85,30,10);
-dkbrown.rect(260,85,30,10);
+dkbrown.rect(210,85,30,12);
+dkbrown.rect(260,85,30,12);
 
 dkbrown.rect(205,110,85,25);
-dkbrown.rect(115,305,35,12);
+dkbrown.rect(115,305,39,15);
 dkbrown.rect(160,305,40,8);
 
 ck = new Drawback('#eca009',1,context);
 ck.circle(410,150,16);
 ck.rect(401,160,18,30);
 ck.rect(398,190,7,20);
-ck.rect(398,190,7,20);
-ck.rect(398,190,7,20);//지울것
-ck.rect(398,190,7,20);//지울것
+ck.rect(416,190,7,20);
+ck.circle(401.5,210,3.5);
+ck.circle(419.5,210,3.5);
+
+ck.rect(390,167,15,7);
+ck.rect(415,167,15,7);
+ck.circle(390,170.5,3.5);
+ck.circle(430,170.5,3.5);
+
+white.rect(380,160,5,30);
+white.rect(370,155,15,5);
+
+//상단바ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+function UpBar(){
+bar = new Drawback('#5b5a4c',2,context);
+
+bar.rect(60,40,60,15);
+bar.rect(140,40,80,15);
+bar.rect(230,40,100,15);
+barwhite = new Drawback('#eeece0',3,context);
+
+barwhite.rect(350,40,100,15);
+
+bargreen = new Drawback('#09c813',1,context);
+
+bargreen.circle(220,47,7);
+bargreen.circle(330,47,7);
+bargreen.circle(450,47,7);
+
+
+
+
+barwhite.text("53%",85,52);
+barwhite.text("923",180,52);
+barwhite.text("15,460",285,52);
+barcyon = new Drawback('#12afb9',1,context);
+
+barcyon.circle(500,47,7);
+
+barred=new Drawback('#dc1414',2,context);
+for(i=0;i<6;i++)barred.circle(380+i*7,48,7);
+}
+
+
+
+
+
+//다시 시자그ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+white.text("에피소드 지도",402,95);
+white.text("오븐 탈출",128,90);
+white.text("0/3",215,94);
+white.text("0/39",264,94);
+white.text("리그랭킹",228,126);
+white.text("추가체력 5 보너스",370,225);
+white.text("펫",370,255);
+white.text("쿠키",392,255);
+white.text("보물",420,255);
+white.text("게임 시작",385,295);
+white.text("랭킹보상",112,316);
+white.text("0/100",165,320);
+
+
+black= new Drawback('#020202',1,context);
+
+black.text("친구 랭킹",130,125);
+black.text("최혁순      422,300",170,168);
+black.text("손흥민       58,298",170,198);
+black.text("리오넬 메시  1,000",170,228);
+black.text("호날두           980",170,258);
+black.text("네이마르        680",170,288);
 
 
 
--- second.js
+++ second.js
@@ -64,73 +64,130 @@ var canvas = document.getElementById('canvas'),
        
      };
  
-     
+     function Drawback(color,fill,context){
+      //배경
+      this.color=color;
+      this.fill=fill;
+      this.context=context;
+  }
+  Drawback.prototype={
+
+      rect: function(x,y,w,h){
+          if(this.fill==0){
+              this.context.strokeStyle=this.color;
+              this.context.beginPath();
+              this.context.strokeRect(x,y,w,h);
+
+          }
+          else if(this.fill==1){
+              this.context.fillStyle=this.color;
+              this.context.beginPath();
+              this.context.fillRect(x,y,w,h);
+
+          }
+          else{
+              this.context.fillStyle=this.color;
+              this.context.beginPath();
+              this.context.fillRect(x,y,w,h);
+              this.context.strokeStyle='#020202';
+              this.context.strokeRect(x,y,w,h);
+
+
+          }
+
+      },
+      circle: function(x,y,rad){
+          if(this.fill==0){
+              this.context.strokeStyle=this.color;
+              this.context.beginPath();
+              this.context.arc(x,y,rad,Math.PI,Math.PI*3);
+              this.context.stroke();
+          }
+          else if(this.fill==1){
+              this.context.fillStyle=this.color;
+              this.context.beginPath();
+              this.context.arc(x,y,rad,Math.PI,Math.PI*3);
+              this.context.fill();
+
+          }
+          else{
+              this.context.fillStyle=this.color;
+              this.context.beginPath();
+              this.context.arc(x,y,rad,Math.PI,Math.PI*3);
+              this.context.fill();
+              this.context.strokeStyle='#020202';
+              this.context.stroke();
+              
+
+          }
+      },
+
+      line: function(x1,y1,x2,y2,width){
+          this.context.lineWidth=width;
+          this.context.strokeStlye=this.color;
+          this.context.beginPath();
+          this.context.moveTo(x1,y1);
+          this.context.lineTo(x2, y2);
+          this.context.stroke();
+      },
+
+      text: function(text,x,y){
+          this.context.font ='12px Helvetica';
+          this.context.fillStyle=this.color;
+          this.context.fillText(text,x,y);      
+      }
+  }
+
+  brown = new Drawback('#c3580d',3,context);
+  white = new Drawback('#eeece0',3,context);
+  gray= new Drawback('#a2a2a2',3,context);
+  cyon = new Drawback('#12afb9',1,context);
+  green = new Drawback('#09c813',1,context);
+  dkbrown = new Drawback('#712e00',1,context);
+  ck = new Drawback('#eca009',1,context);
+  black= new Drawback('#020202',1,context);
 
 
 
 
 
 
-
-function drawback(){
-    //배경
-    context.strokeStyle='#020202';
-    context.fillStyle='#5e4040';
+     function UpBar(){
+      context.strokeStyle='#020202';
+      context.fillStyle='#a03f4e';
     
 
-    context.fillRect(25, 25, 500, 300);
-
-    context.strokeStyle='#9d9d9d';
-
-    context.strokeRect(80, 35, 50, 20);
-    context.strokeRect(150, 35, 70, 20);
-    context.strokeRect(240, 35, 120, 20);
-
-    context.fillStyle='#f2f2f2';
-    context.fillRect(370, 35, 120, 20);
-    context.fillStyle='#f4e111';
-    context.beginPath();
-    context.arc(78, 44, 8, Math.PI , Math.PI*3 );
-    context.fill();
-
-    context.fillStyle='#a8a8a5';
-    context.beginPath();
-    context.arc(238, 44, 8, Math.PI , Math.PI*3 );
-    context.fill();
-
-    context.fillStyle='#46bd14';
-    context.beginPath();
-    context.arc(220, 44, 8, Math.PI , Math.PI*3 );
-    context.fill();
-    context.beginPath();
-    context.arc(360, 44, 8, Math.PI , Math.PI*3 );
-    context.fill();
-    context.beginPath();
-    context.arc(490, 44, 8, Math.PI , Math.PI*3 );
-    context.fill();
-
-    context.fillStyle='#f83333';
-    context.strokeStyle='#020202';
-    
-    for(i=0;i<=5;i++){
-    context.beginPath();
-    context.arc(420+i*(3+8), 44, 8, Math.PI , Math.PI*3 );
-    context.fill();
-context.stroke();
-    }
-
-context.font ='18px Helvetica';
-context.fillStyle='white';
-
-context.fillText("53%",90,50);  
-context.fillText("923",180,50);  
-context.fillText("15,460",270,50);  
-context.fillStyle='#020202';
-
-context.fillText("13",380,50); 
-}
-drawback();
-
+      context.fillRect(25, 25, 500, 300);
+      context.stroke();
+      context.fill();
+      bar = new Drawback('#5b5a4c',2,context);
+      
+      bar.rect(60,40,60,15);
+      bar.rect(140,40,80,15);
+      bar.rect(230,40,100,15);
+      barwhite = new Drawback('#eeece0',3,context);
+      
+      barwhite.rect(350,40,100,15);
+      
+      bargreen = new Drawback('#09c813',1,context);
+      
+      bargreen.circle(220,47,7);
+      bargreen.circle(330,47,7);
+      bargreen.circle(450,47,7);
+      
+      
+      barwhite.text("53%",85,52);
+      barwhite.text("923",180,52);
+      barwhite.text("15,460",285,52);
+      barcyon = new Drawback('#12afb9',1,context);
+      
+      barcyon.circle(500,47,7);
+      
+      barred=new Drawback('#dc1414',2,context);
+      for(i=0;i<6;i++)barred.circle(380+i*7,48,7);
+      }
+      
+UpBar();
 context.beginPath();
 
 
@@ -140,33 +197,33 @@ context.fillStyle='white';
 
 
 
-context.fillStyle='#e3e3e1';//왼쪽
-context.fillRect(80, 70, 170, 200);
-context.fillStyle='#2082e7';
-context.fillRect(95-distance, 100-distance, iconwidth*3+distance*4, iconwidth+distance*2);
-context.fillStyle='#50a0f2';
-for(i=0;i<3;i++) context.fillRect(95+i*(iconwidth+distance), 100, iconwidth, iconwidth);
 
-context.fillStyle='#359e38';
-context.fillRect(95-distance, 100+iconwidth+5*distance, iconwidth*3+distance*4, iconwidth*2+distance*3);
-context.fillStyle='#4cdf4f';
-for(i=0;i<3;i++) context.fillRect(95+i*(iconwidth+distance), 100+iconwidth+6*distance, iconwidth, iconwidth);
-for(i=0;i<3;i++) context.fillRect(95+i*(iconwidth+distance), 100+2*iconwidth+7*distance, iconwidth, iconwidth);
-
-context.strokeStyle='#4d51fa';//오른쪽
-context.fillStyle='#50a0f2';
-context.strokeRect(80+170+10*distance,70+4*distance,170,150);
-context.fillRect(80+170+13*distance, 70+7*distance, iconwidth, iconwidth);//아이템
-context.fillText("Lv.2",80+170+15*distance, 70+10*distance);   
+white.rect(80, 70, 170, 200);
 
 
-context.fillStyle='#b3b3b7';
-context.arc(80+170+20*distance+iconwidth, 70+12*distance, 5, Math.PI,Math.PI*3);//동전
-context.fill();
-context.font ='18px Helvetica';
-context.fillStyle='white';
 
-context.fillText("체력강화 Lv.2",250+15*distance+iconwidth, 70+9*distance);  
+dpblue=new Drawback("#2082e7",1,context);
+dpblue.rect(95-distance, 100-distance, iconwidth*3+distance*4, iconwidth+distance*2)
+
+blue=new Drawback("#50a0f2",1,context);
+for(i=0;i<3;i++) blue.rect(95+i*(iconwidth+distance), 100, iconwidth, iconwidth);
+
+
+green.rect(95-distance, 100+iconwidth+5*distance, iconwidth*3+distance*4, iconwidth*2+distance*3);
+
+wkgreen=new Drawback("#9cdd78",1,context);
+for(i=0;i<3;i++) wkgreen.rect(95+i*(iconwidth+distance), 100+iconwidth+6*distance, iconwidth, iconwidth);
+for(i=0;i<3;i++) wkgreen.rect(95+i*(iconwidth+distance), 100+2*iconwidth+7*distance, iconwidth, iconwidth);
+
+
+navy=new Drawback("#4d51fa",0,context);
+navy.rect(80+170+10*distance,70+4*distance,170,150);
+
+blue.rect(80+170+13*distance, 70+7*distance, iconwidth, iconwidth);//아이템
+
+
+
+gray.circle(80+170+20*distance+iconwidth, 70+12*distance, 5);//동전
 
 
 context.font ='12px Helvetica';
@@ -180,45 +237,44 @@ context.fillText("Lv.2",80+170+15*distance, 70+14*distance);
 
 
 
-context.beginPath();//업그레이드버튼
-context.fillStyle='#00dce3';
-context.arc(250+17*distance+iconwidth, 70+18*distance, 10, Math.PI,Math.PI*3);
-context.arc(300+17*distance+iconwidth, 70+18*distance, 10, Math.PI,Math.PI*3);
-context.fill();
-context.fillRect(250+17*distance+iconwidth,70+18*distance-10,50,20);
-context.font ='12px Helvetica';
-context.fillStyle='white';
-context.fillText("업그레이드",250+17*distance+iconwidth,70+18*distance+4);   
-context.fillText("추가체력이 총 10 늘어납니다.",250+5*distance+iconwidth,70+26*distance+4);   
-context.fillText("모든 쿠키들에게 적용됩니다.",250+5*distance+iconwidth,70+29*distance+4);   
+
+
+cyon.circle(250+17*distance+iconwidth, 70+18*distance, 10);
+cyon.circle(300+17*distance+iconwidth, 70+18*distance, 10);
+cyon.rect(250+17*distance+iconwidth,70+18*distance-10,50,20);
+
+
+
+white.text("업그레이드",250+17*distance+iconwidth,70+18*distance+4);   
+white.text("추가체력이 총 10 늘어납니다.",250+5*distance+iconwidth,70+26*distance+4);   
+white.text("모든 쿠키들에게 적용됩니다.",250+5*distance+iconwidth,70+29*distance+4);   
 
 
 
 
 
-context.beginPath();//게임시작버튼
-context.fillStyle='#54e110';
-context.arc(330, 265, 16, Math.PI,Math.PI*3);
-context.arc(430,265, 16, Math.PI,Math.PI*3);
-context.fill();
-context.fillRect(330, 265-16,100,32);
+green.circle(330, 265, 16);
+green.circle(430,265, 16);
+green.rect(330, 265-16,100,32);
 
 
 context.font ='24px Helvetica';
 context.fillStyle='white';
 context.fillText("게임 시작",336, 272);   
 
+context.font ='18px Helvetica';
+context.fillStyle='white';
+context.fillText("체력강화 Lv.2",250+15*distance+iconwidth, 70+9*distance);  
 
 
-context.fillStyle='#020202';
-context.font ='15px Helvetica';
-context.fillText("능력을 업그레이드하세요",85,90); 
-context.fillText("부스트를 구매하세요",85,163); 
 
-context.fillStyle='#8d8d8d';
-    context.beginPath();
-    context.arc(250, 75, 9, Math.PI , Math.PI*3 );
-    context.fill();
+
+black.text("능력을 업그레이드하세요",93,90); 
+black.text("부스트를 구매하세요",93,163); 
+
+
+    gray.circle(250, 75, 9);
+
 
     canvas.onmousedown = function (i) {
         var loc = windowToCanvas(i.clientX, i.clientY);
